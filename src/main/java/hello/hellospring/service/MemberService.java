9ab1d575d38308260cc7c462f2fd591ecda4b1d6
zenibako.lee@gmail.com <zenibako.lee@gmail.com>
2023-09-07 22:18:40 +0900

    spring bean component set with annotation

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,15 +3,22 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+    @Autowired // memberRepository Dependency Injection
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository; // Dependency Injection
+    }
+
     /*
-    회원가입
-     */
+        회원가입
+         */
     public Long join(Member member){
         validateDuplicatedMember(member); // ctrl+t & extract method
         memberRepository.save(member);
