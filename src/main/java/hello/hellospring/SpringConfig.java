c678b3dce144f18ddecb71964caa0721ce6369bb
zenibako.lee@gmail.com <zenibako.lee@gmail.com>
2023-11-05 22:50:05 +0900

    aop

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,10 +1,7 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JpaMemberRespository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,31 +9,41 @@ import org.springframework.context.annotation.Configuration;
 public class SpringConfig {
 
     //    private DataSource dataSource;
-//
 
 //    @Autowired // DataSource를 SpringConfig 클래스의 생성자에 주입
 //    public SpringConfig(DataSource dataSource) {
 //        this.dataSource = dataSource;
 //    }
 
-    private EntityManager em;
+//    private EntityManager em;
+//
+//    @Autowired // DataSource를 SpringConfig 클래스의 생성자에 주입
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
-    @Autowired // DataSource를 SpringConfig 클래스의 생성자에 주입
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+//        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRespository(em);
-    }
+    //    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRespository(em);
+//    }
+//    @Bean
+//    public TimeTraceApp tImeTraceAop() {
+//        return new TimeTraceApp();
+//    }
 }
